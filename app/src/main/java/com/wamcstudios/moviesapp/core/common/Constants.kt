dee3db763eb559cbc879ccc50107fd95ad38f779
wilmermolinac <wilmermolinac@gmail.com>
2024-04-15 20:46:23 -0500

    inicialized project

--- app/src/main/java/com/wamcstudios/moviesapp/core/common/Constants.kt
+++ app/src/main/java/com/wamcstudios/moviesapp/core/common/Constants.kt
@@ -4,6 +4,9 @@ object Constants {
 
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+
+    // Your api key Here!
+    // https://developer.themoviedb.org/docs/getting-started
     const val API_KEY = "c8927a919b98967d82458b648adda360"
 
     const val TRENDING_TIME = "day"
